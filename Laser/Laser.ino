48819e21b3e7abddc4fc2d873876f21ad14af2f1
Andy Lu <andylu@DESKTOP-ICMNABF.localdomain>
2020-03-01 21:23:31 -0800

    MQTT conversion

--- Laser/Laser.ino
+++ Laser/Laser.ino
@@ -1,8 +1,12 @@
 #include <ESP8266WiFi.h>
-#include <ArduinoWebsockets.h>
+#include <PubSubClient.h>
 
-const char* ssid = "G7 ThinQ_8654";
-const char* password = "Aa1779144";
+const char* ssid = "HitronAL2.4";
+const char* password = "lu19920403";
+const char* mqtt_server = "postman.cloudmqtt.com";
+const int mqtt_port = 15145;
+const char* mqtt_username = "yrahqccv";
+const char* mqtt_password = "IJBjDBMUekyr";
 
 const int ldrPin = A0;
 const int laser = 5;
@@ -10,41 +14,8 @@ const int armedLed = 0;
 int ldrBase;
 boolean armed = false;
 
-using namespace websockets;
-WebsocketsClient client;
-
-void onMessageCallback(WebsocketsMessage message) {
-  Serial.print("Got Message: ");
-  Serial.println(message.data());
-  if (message.data() == "on") {
-    armed = true;
-    digitalWrite(armedLed, HIGH);
-    ldrBase = analogRead(ldrPin);
-    delay(1000);
-    digitalWrite(laser, HIGH);
-  } else if (message.data() == "off") {
-    armed = false;
-    digitalWrite(laser, LOW);
-    digitalWrite(armedLed, LOW);
-  }
-}
-
-void onEventsCallback(WebsocketsEvent event, String data) {
-  if (event == WebsocketsEvent::ConnectionOpened) {
-    Serial.println("Connnection Opened");
-    client.send("{\"event\":\"add_device\",\"name\":\"Doorway Laser\",\"type\":\"Laser\",\"mac\":\"" + WiFi.macAddress() + "\",\"status\":false}");
-  } else if (event == WebsocketsEvent::ConnectionClosed) {
-    Serial.println("Connnection Closed");
-  } else if (event == WebsocketsEvent::GotPing) {
-    Serial.println("Got a Ping!");
-    client.send("{\"event\":\"ping_response\",\"mac\":\"" + WiFi.macAddress() + "\"}");
-  } else if (event == WebsocketsEvent::GotPong) {
-    Serial.println("Got a Pong!");
-  }
-}
-
-
-
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
   // put your setup code here, to run once:
@@ -67,23 +38,49 @@ void setup() {
   Serial.print("IP: ");
   Serial.print(WiFi.localIP());
   Serial.println("");
-  client.onMessage(onMessageCallback);
-  client.onEvent(onEventsCallback);
-  client.connect("ws://192.168.43.122:3000/ws");
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+ 
+  while (!client.connected()) {
+    Serial.println("Connecting to MQTT...");
+    if (client.connect("ESP8266Client", mqtt_username, mqtt_password )) {
+      Serial.println("connected");
+      ldrBase = analogRead(ldrPin);
+    } else {
+      Serial.print("failed with state ");
+      Serial.print(client.state());
+      delay(2000);
+    }
+  }
+
+  client.publish("/connectionEvents", "Connected Laser"); //Topic name
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+ 
+  Serial.print("Message arrived in topic: ");
+  Serial.println(topic);
+  Serial.print("Message:");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  Serial.println("-----------------------");
 }
 
 void loop() {
-  client.poll();
+  client.loop();
 //put your main code here, to run repeatedly:
   int ldrVal = analogRead(ldrPin);
   //  Serial.println(ldrBase);
   //  Serial.println(ldrVal);
-  if (armed) {
-    if (ldrVal - 50 < ldrBase) {
-      client.send("{\"event\":\"device_event\",\"name\":\"Doorway Laser\",\"type\":\"Laser\",\"mac\":\"" + WiFi.macAddress() + "\",\"message\":\"Laser tripped.\"}");
+//  if (armed) {
+    if (ldrVal < ldrBase) {
+      client.publish("/systemEvents", "Laser triggered.");
       Serial.println("Intruder!");
       delay(3000);
     }
-  }
+//  }
   delay(250);
 }
--- Server/server.js
+++ Server/server.js
@@ -1,30 +1,62 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const expressws = require("express-ws")(app);
+const bodyParser = require('body-parser');
+const expressws = require('express-ws')(app);
+const mqtt = require('mqtt');
 const PORT = process.env.PORT || 3000;
-const { Client } = require("pg");
+const { Client } = require('pg');
 const {
   login,
   system_event,
   retrieve_events
-} = require("./Database/functions");
+} = require('./Database/functions');
 let ApplicationSockets = [];
 let DeviceSockets = [];
 
+const getServerMessage = message => {
+  return `<${new Date().toLocaleString()}> ${message}`;
+};
+
 const connection = new Client({
-  host: "localhost",
+  host: 'localhost',
   port: 5432,
-  database: "guardpi",
-  user: "guardpi_user",
-  password: "XeqLLSLkl3IcgsYp"
+  database: 'guardpi',
+  user: 'guardpi_user',
+  password: 'XeqLLSLkl3IcgsYp'
+});
+
+const client = mqtt.connect('mqtts://postman.cloudmqtt.com:25145', {
+  username: 'yrahqccv',
+  password: 'IJBjDBMUekyr'
+});
+
+client.on('connect', function() {
+  console.log(`${getServerMessage('CloudMQTT connected.')}`);
+});
+
+client.subscribe('/sensorEvents', () => {
+  client.on('message', (topic, msg, pkt) => {
+    console.log(topic, msg.toString('utf8'), pkt);
+  });
+});
+
+client.subscribe('/systemEvents', () => {
+  client.on('message', (topic, msg, pkt) => {
+    console.log(topic, msg.toString('utf8'), pkt);
+  });
+});
+
+client.subscribe('/connectionEvents', () => {
+  client.on('message', (topic, msg, pkt) => {
+    console.log(topic, msg.toString('utf8'), pkt);
+  });
 });
 
 connection.connect(err => {
   if (err) {
-    console.error("connection error", err.stack);
+    console.error('connection error', err.stack);
   } else {
-    console.log("Database connected.");
+    console.log(`${getServerMessage('Database connected.')}`);
   }
 });
 
@@ -34,166 +66,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-const cleanupSockets = setInterval(() => {
-  // console.log(Math.random(), deviceSockets);
-  if (DeviceSockets.length > 0) {
-    let aliveSockets = DeviceSockets.filter(device => device.isAlive === true);
-    if (aliveSockets.length > 0) {
-      aliveSockets.map(socket => {
-        socket.isAlive = false;
-        if (socket.ws.readyState === 1) {
-          socket.ws.ping();
-        }
-      });
-    }
-    DeviceSockets = [...aliveSockets];
-  }
-  setTimeout(() => {
-    if (ApplicationSockets.length > 0) {
-      let Sockets = [];
-      if (DeviceSockets.length > 0) {
-        DeviceSockets.map(device => {
-          Sockets.push({
-            name: device.name,
-            type: device.type,
-            mac: device.mac,
-            status: device.status
-          });
-        });
-      }
-      ApplicationSockets.map(application => {
-        if (application.ws.readyState === 1) {
-          application.ws.send(
-            JSON.stringify({ event: "updated_devices", devices: Sockets })
-          );
-        }
-      });
-    }
-  }, 1000);
-  console.log(DeviceSockets);
-}, 10000);
-
-app.post("/login", function(req, res) {
+app.post('/login', function(req, res) {
   const callback = data => {
     res.send(data);
   };
   login(connection, req.body.username, req.body.userPass, callback);
 });
 
-app.get("/events", (req, res) => {
+app.get('/events', (req, res) => {
   const callback = data => {
     res.send(data);
   };
   retrieve_events(connection, callback);
 });
 
-const wsInstance = expressws.getWss("/ws");
-
-app.ws("/ws", (ws, req) => {
-  ws.on("message", msg => {
-    let data = JSON.parse(msg);
-    console.log(data);
-    switch (data.event) {
-      case "add_application":
-        ApplicationSockets.push({ token: data.token, ws: ws });
-        console.log(data);
-        break;
-      case "retrieve_devices":
-        let Sockets = [];
-        if (DeviceSockets.length > 0) {
-          DeviceSockets.map(device => {
-            Sockets.push({
-              name: device.name,
-              type: device.type,
-              mac: device.mac,
-              status: device.status
-            });
-          });
-        }
-        ws.send(JSON.stringify({ event: "updated_devices", devices: Sockets }));
-        break;
-      case "add_device":
-        let skip = false;
-        if (DeviceSockets.length > 0) {
-          DeviceSockets.map(device => {
-            if (data.mac === device.mac) {
-              device.ws = ws;
-              skip = true;
-              setTimeout(() => {
-                sendUpdates();
-              }, 1000);
-            }
-          });
-        }
-        if (!skip) {
-          console.log(data);
-          DeviceSockets.push({
-            name: data.name,
-            type: data.type,
-            mac: data.mac,
-            status: data.status,
-            ws: ws,
-            isAlive: true
-          });
-          setTimeout(() => {
-            sendUpdates();
-          }, 1000);
-        }
-        break;
-      case "device_status":
-        if (DeviceSockets.length > 0) {
-          DeviceSockets.map(device => {
-            if (device.mac === data.mac) {
-              device.status = data.status;
-              device.ws.send(data.status ? "on" : "off");
-            }
-          });
-          setTimeout(() => {
-            sendUpdates();
-          }, 1000);
-        }
-        console.log(data);
-        break;
-      case "device_event":
-        console.log(data);
-        system_event(connection, data.name, data.type, data.mac, data.message);
-      case "ping_response":
-        if (DeviceSockets.length > 0) {
-          DeviceSockets.map(device => {
-            if (device.mac === data.mac) {
-              device.isAlive = true;
-            }
-          });
-        }
-      default:
-        break;
-    }
-  });
-});
-
-const sendUpdates = () => {
-  if (ApplicationSockets.length > 0) {
-    let Sockets = [];
-    if (DeviceSockets.length > 0) {
-      DeviceSockets.map(device => {
-        Sockets.push({
-          name: device.name,
-          type: device.type,
-          mac: device.mac,
-          status: device.status
-        });
-      });
-    }
-    ApplicationSockets.map(application => {
-      if (application.ws.readyState === 1) {
-        application.ws.send(
-          JSON.stringify({ event: "updated_devices", devices: Sockets })
-        );
-      }
-    });
-  }
-};
-
 app.listen(PORT, () => {
-  console.log(`Server is up on ${PORT}`);
+  console.log(`${getServerMessage(`Server up on PORT ${PORT}.`)}`);
 });
